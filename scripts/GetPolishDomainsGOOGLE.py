65ed53ba0358215f66760edfadd825858774ef41
eaglx <eaglx.programmer@gmail.com>
2019-03-10 18:24:42 +0100

    :hammer: Create scripts

--- scripts/GetPolishDomainsGOOGLE.py
+++ scripts/GetPolishDomainsGOOGLE.py
@@ -11,15 +11,13 @@ import re
 import bs4
 from googlesearch import search
 
-print("START: GetPolishDomains")
-print(datetime.datetime.now())
-print("************************")
+print("******************************************")
+print("START: GetPolishDomainsGOOGLE ", datetime.datetime.now())
+print("******************************************")
 
-PolishDomains = [] # STORE POLISH DOMAINS
+fileDomain = open("../isidb/pldbgs.data", 'a')
 
 # ***********************************************************
-print("FIRST STEP: GET URLS FROM GOOGLE")
-query = "site:pl"
 # search():
 # query : query string that we want to search for.
 # tld : tld stands for top level domain which means we want to search our result on google.com or google.in or some other domain.
@@ -29,12 +27,20 @@ query = "site:pl"
 # stop : Last result to retrieve. Use None to keep searching forever.
 # pause : Lapse to wait between HTTP requests. Lapse too short may cause Google to block your IP. Keeping significant lapse will make your program slow but its safe and better option.
 # Return : Generator (iterator) that yields found URLs. If the stop parameter is None the iterator will loop forever.
-for j in search(query, tld="com", lang="pl", num=5, start=0, stop=10, pause=6):
-    print(j) # j is just a string
-    temp1 = re.search('//(.+)/', j).group(1)
-    try:
-        temp2 = re.search('www.(.+)', temp1).group(1)
-        PolishDomains.append(temp2)
-    except AttributeError:
-        PolishDomains.append(temp1)
+try:
+    for urlsFromG in search("site:pl", tld="com", lang="pl", num=10, start=0, stop=None, pause=10):
+        print(' >', urlsFromG) # j is just a string
+        temp1 = ''
+        try:
+            temp1 = re.search('//(.+)/', urlsFromG).group(1)
+        except AttributeError:
+            fileDomain.write(urlsFromG + '\n')
+            continue
+        try:
+            temp2 = re.search('www.(.+)', temp1).group(1)
+            fileDomain.write(temp2 + '\n')
+        except AttributeError:
+            fileDomain.write(temp1 + '\n')
+except KeyboardInterrupt:
+    fileDomain.close()
 # ***********************************************************
