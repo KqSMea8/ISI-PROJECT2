b2dfccdc8b9d13c620dd7fdad1a35df4186086c4
eaglx <eaglx.programmer@gmail.com>
2019-03-30 18:55:30 +0100

    Add robot.txt downloader

---
+++ scripts/GetRobotTXT.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys
+import requests
+
+if len(sys.argv) != 2:
+    print("Few args!")
+    print("GetRobotTXT.py URL")
+
+getRoboTXTfromURL = sys.argv[1]
+
+response = requests.get(getRoboTXTfromURL + "/robots.txt")
+
+file_name = ""
+
+if getRoboTXTfromURL[0:5] == "http:":
+    file_name = getRoboTXTfromURL[7:getRoboTXTfromURL[0:4].find(".pl")-2]
+elif getRoboTXTfromURL[0:5] == "https":    
+    file_name = getRoboTXTfromURL[8:getRoboTXTfromURL[0:4].find(".pl")-2]
+else:
+    file_name = getRoboTXTfromURL[0:getRoboTXTfromURL[0:4].find(".pl")-2]
+
+f = open('robot/'+file_name+'.robot', 'w')
+f.write(response.text)
+f.close
