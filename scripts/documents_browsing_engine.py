92b9fccad2cc03606048e03f2bd59ff63c05d94e
Krzysztof Jurkiewicz <huntekah@gmail.com>
2019-05-12 13:48:33 +0200

    creating model

--- scripts/documents_browsing_engine.py
+++ scripts/documents_browsing_engine.py
@@ -4,7 +4,7 @@ import os
 import nltk
 from string import punctuation
 robot_dir = "../RobotsFiles/"
-robot_dir = "../testRobotsFiles/"
+# robot_dir = "../testRobotsFiles/"
 from sklearn.linear_model import SGDClassifier
 import pickle
 from multiprocessingTools import get_job_results_and_show_statistics
@@ -13,7 +13,9 @@ from multiprocessing import Pool
 
 from inspect import currentframe
 
-cf = currentframe()
+def print_lineno():
+    cf = currentframe()
+    print("line number:",cf.f_lineno)
 
 def load_model(filename):
     with open(filename, 'rb') as handle:
@@ -37,45 +39,46 @@ def _preprocess_document(document):
     preprocessed_document = " ".join(words)
     return preprocessed_document
 
+
+def _nonblocking_document_processing(content) -> str():
+    preprocessed_document = _preprocess_document(content)
+    return preprocessed_document
+
+
 def load_robots_txt_files() -> list():
-    def _nonblocking_document_processing(file_handle) -> str():
-        pass
-    cf = currentframe()
     documents = []
+    filenames = []
     pool = mp.Pool(mp.cpu_count())
     jobs = []
-
-
-    print(cf.f_lineno)
+    print_lineno()
 
     for filename in os.listdir(robot_dir):
+        print("reading  ",filename)
         with open(os.path.join(robot_dir,filename), mode="r", encoding="utf8") as robots_txt:
             content = robots_txt.read()
-            preprocessed_document = _preprocess_document(content)
-            documents.append((filename,preprocessed_document))
-    print(cf.f_lineno)
-    return documents
+        job = pool.apply_async(_nonblocking_document_processing,(content,))
+        jobs.append(job)
+        filenames.append(filename)
+    documents = get_job_results_and_show_statistics(jobs,job_name="load_documents", sleep_time=30)
+    #documents.append((filename,preprocessed_document))
+    print_lineno()
+    return documents, filenames
 
 def create_model() -> dict():
-    cf = currentframe()
-    documents = load_robots_txt_files()
-    print(cf.f_lineno)
-    train_X = [content for _,content in documents]
-    print(cf.f_lineno)
-    train_Y = [filename for filename,_ in documents]
-    documents.clear()
-    print(cf.f_lineno)
+    train_X, train_Y = load_robots_txt_files()
+    print_lineno()
 
-    vectorizer = TfidfVectorizer(max_df=0.999, min_df=0.001, max_features=50)
-    print(cf.f_lineno)
+    vectorizer = TfidfVectorizer(max_df=0.999, min_df=0.001, max_features=50000)
+    print_lineno()
     model_X = vectorizer.fit_transform(train_X)
 
-    model = SGDClassifier(loss='hinge', penalty='l2',
+    model = SGDClassifier(loss='modified_huber', penalty='l2',
                           alpha=1e-3, random_state=42,
-                          max_iter=5, tol=None)
-    print(cf.f_lineno)
+                          max_iter=10, tol=1e-3, verbose=2)
+    print_lineno()
     model.fit(model_X,train_Y)
-    return model, vectorizer
+    print_lineno()
+    return model, vectorizer, model_X, train_Y
 
 
 def find_best_match(document):
@@ -89,21 +92,33 @@ def save_model(model, filename):
         pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
-    trained_model, vectorizer = create_model()
+    trained_model, vectorizer, model_X, train_Y = create_model()
 
-    #test_doc = "kalendarium www admin kupa"
-    #proc_doc = _preprocess_document(test_doc)
-    #print(proc_doc)
-    #vectorized_doc = vectorizer.transform([proc_doc])
-    #print(trained_model.predict(vectorized_doc))
+    # test_doc = "kalendarium www admin kupa"
+    # proc_doc = _preprocess_document(test_doc)
+    # print(proc_doc)
+    # vectorized_doc = vectorizer.transform([proc_doc])
+    # print(trained_model.predict(vectorized_doc))
+    # # print(trained_model.predict_log_proba(vectorized_doc))
+    # # probas = trained_model.predict_log_proba(vectorized_doc)[0]
+    # # for prob, name in zip(probas,trained_model.classes_):
+    # #     print(name, prob)
+    # # # print(trained_model.predict_proba(vectorized_doc))
+    # for X, name in zip(model_X, trained_model.classes_):
+    #     print(name, cosine_distances(X,vectorized_doc))
+    #     #print(name)
+    # # print(trained_model.predict_proba(vectorized_doc))
 
 
-    saving = False
+
+    saving = True
     if saving:
-        print(cf.f_lineno)
-        save_model(trained_model, "model.pickle")
+        print_lineno()
+        save_model(trained_model, "SGD_model.pickle")
         save_model(vectorizer, "vectorizer.pickle")
-        m1 = load_model("model.pickle")
-        m2 = load_model("vectorizer.pickle")
-        print(cf.f_lineno)
+        save_model(model_X, "vectorizer_features.pickle")
+        save_model(train_Y, "vectorizer_classes.pickle")
+        # m1 = load_model("_SGD_model.pickle")
+        # m2 = load_model("_SGD_vectorizer.pickle")
+        print_lineno()
         print("end")
