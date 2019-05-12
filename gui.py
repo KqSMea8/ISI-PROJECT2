90a0a1dbfd06050951c61341099aa7533ab6179d
eaglx <eaglx.programmer@gmail.com>
2019-05-12 13:29:57 +0200

    Update and fix gui.py

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import sys
 
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot, QByteArray
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QTextEdit, QLabel, QListWidgetItem, QListWidget, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QTextEdit, QLabel, QListWidgetItem, QListWidget, QHBoxLayout, QRadioButton, QScrollArea
 from PyQt5.QtGui import QIcon, QMovie
 
 
@@ -22,6 +22,13 @@ class Gui(QWidget):
         self.searchButton.setToolTip('click to run your search')
         self.searchButton.clicked.connect(self.search)
 
+        self.sgdclfiButton = QRadioButton("Klasyfikator SGD")
+        self.sgdclfiButton.setChecked(True)
+        self.sgdclfiButton.setToolTip("kosinus kąta pomiędzy dwoma wektorami reprezentującymi dokumentów")
+
+        self.distcosButton = QRadioButton("Dystans Kosinusowy")
+        self.distcosButton.setToolTip("klasyfikator liniowy wykorzystujący Stochastic Gradient Descent")
+
         self.queryTextEdit = QTextEdit()
         self.queryTextEdit.setToolTip('type in your query')
         self.queryTextEdit.setMaximumHeight(25)
@@ -32,7 +39,12 @@ class Gui(QWidget):
         vbox = QVBoxLayout()
         vbox.addWidget(self.infoLabel, alignment=QtCore.Qt.AlignLeft)
         vbox.addLayout(vbox2)
-        vbox.addWidget(self.searchButton, alignment=QtCore.Qt.AlignRight)
+
+        hbox2 = QHBoxLayout()
+        hbox2.addWidget(self.sgdclfiButton)
+        hbox2.addWidget(self.distcosButton)
+        hbox2.addWidget(self.searchButton)
+        vbox.addLayout(hbox2)
 
         self.loading = QLabel()
         vbox.addWidget(self.loading, alignment=QtCore.Qt.AlignCenter)
@@ -42,7 +54,11 @@ class Gui(QWidget):
         self.resultList.currentItemChanged.connect(self.setFileContent)
         hbox.addWidget(self.resultList)
         self.fileContent = QLabel()
-        hbox.addWidget(self.fileContent)
+        self.fileContent.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+        self.contentScroll = QScrollArea()
+        self.contentScroll.setWidgetResizable(True)
+        self.contentScroll.setWidget(self.fileContent)
+        hbox.addWidget(self.contentScroll)
         vbox.addLayout(hbox)
 
         self.setLayout(vbox)
@@ -50,22 +66,24 @@ class Gui(QWidget):
 
     @pyqtSlot()
     def search(self):
-        print("searching")
         # TODO search functionality
         self.movie = QMovie("./loader.gif", QByteArray(), self)
         self.loading.setMovie(self.movie)
         self.movie.start()
         # TODO stop when data are ready
-        self.setSearchResults(["nana", "sialala", "blabla"])
+        self.setSearchResults(["www.darland.pl", "zsk.poznan.pl", "zbawiciel.poznan.pl", "www.zsk.poznan.pl", "wwww.epoznan.pl", "www.volkswagen-poznan.pl", "www.put.poznan.pl"])
 
     def setSearchResults(self, results):
         for r in results:
             newItem = QListWidgetItem()
             newItem.setText(r)  # TODO adjust to result structure
             self.resultList.insertItem(0, newItem)
+        self.resultList.setMinimumWidth(self.resultList.sizeHintForColumn(0))
+        self.resultList.setMaximumWidth(self.resultList.sizeHintForColumn(0)+20)
 
     def setFileContent(self, item):
-    	self.fileContent.setText(item.text())
+        text = open("RobotsFiles/" + item.text(), "r").read()
+        self.fileContent.setText(text)  # TODO compare with fun-list and bold with '<b>...</b>'
 
 
 def run():
